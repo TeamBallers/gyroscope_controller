cd9807c4ce6f2bbc94a91057a05af061eef7830d
Josh Wu <66445796+JoshWho-cpu@users.noreply.github.com>
2026-04-08 14:24:47 -0500

    Replace LSM6DSOX with ISM330DHCX sensor

--- sample.py
+++ sample.py
@@ -1,9 +1,9 @@
 import time
 import board
-from adafruit_lsm6ds.lsm6dsox import LSM6DSOX
+from adafruit_lsm6ds.ism330dhcx import ISM330DHCX
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
-sox = LSM6DSOX(i2c)
+sox = ISM330DHCX(i2c)
 
 while True:
     print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2"%(sox.acceleration))
