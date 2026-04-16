3d8f8496c58ad5daf70dcd02c1171a80e6e9739b
jbw0199-tamu <joshwu820@gmail.com>
2026-04-16 18:06:20 -0500

    add imports to test.py

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 from camera_down_detector import CameraDownDetector
+import board
+from adafruit_lsm6ds.ism330dhcx import ISM330DHCX
+
+i2c = board.I2C()
+sensor = ISM330DHCX(i2c) # you can add a second parameter for the address if needed
+
+sensor.gyro_range = 4000 # set the gyro range to 4000 dps
+sensor.accelerometer_range = 2 # set the accel range to 2 g
 
 if __name__ == "__main__":
     
@@ -9,10 +17,10 @@ if __name__ == "__main__":
     )
 
     
-    detector.initialize_from_stationary(imu.acceleration)  # call once while still
+    detector.initialize_from_stationary(sensor.acceleration)  # call once while still
 
     while True:
-        result = detector.update(imu.acceleration, imu.gyro, dt=0.01)
+        result = detector.update(sensor.acceleration, sensor.gyro, dt=0.01)
         for i in range(3):
             print(f"Camera {i} facing down: {result[i]}", end=" ")
         # result[i] is True if camera i faces downward
